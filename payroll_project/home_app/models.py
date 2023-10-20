d1781439f91b4271f6bcf4303031766e3278b4fe
Sarthak Negi <sarthaknegi7777@gmail.com>
2023-10-20 13:38:24 +0530

    added a model

--- payroll_project/home_app/models.py
+++ payroll_project/home_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class admin_tb(models.Model) :
+    username = models.CharField(max_length=100, primary_key=True)
+    password = models.CharField(max_length=100)
+
+class employee_tb(models.Model) :
+    emp_id = models.IntegerField(primary_key=True)
+    emp_name = models.CharField(max_length=100)
+    emp_job = models.CharField(max_length=100)
+    emp_role = models.CharField(max_length=100)
+    emp_salary = models.IntegerField()
